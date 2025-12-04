03022dd4108d6e2910fb75965acbc172001482a2
KippRei <45483092+KippRei@users.noreply.github.com>
2025-12-03 21:29:25 -0800

    updated pyi file

--- zmkr_audio_engine.pyi
+++ zmkr_audio_engine.pyi
@@ -1,5 +1,43 @@
-from typing import Any
+from typing import Dict, List, Optional, Any, Union
+import numpy as np
+from numpy.typing import NDArray
+
+# Mixer class implemented in Rust
 class Mixer:
-    def __init__(self) -> None: ...
-    def load_data(self, song_name: str, data: Any) -> None: ...
-    def get_map_info(self) -> str: ...
+    # Initializes Mixer
+    def __init__(self) -> None:...
+
+    # Loads preprocessed songs into song dictionary
+    def load_preprocessed_song(
+        self,
+        song_name: str,
+        track_name: str,
+        data: NDArray[np.float64],
+    ) -> None:...
+
+    # Returns list of songs loaded into song dictionary
+    def get_song_list(self) -> List[str]:...
+
+    # Prints song dictionary (used for debugging)
+    def print_song_map(self) -> None:...
+
+    # Loads track/stem into specified channel
+    def load_track(self, title: str, channel: int) -> bool:...
+
+    # Turns channel "on" or "off"
+    def channel_on_off(self, channel: int) -> bool:...
+
+    # Adjusts volume of drum, bass, melody, or vocal channels
+    def adj_track_vol(self, track: str, adjustment: float) -> None:...
+
+    # Returns volume of specified channel type
+    def get_track_vol(self, track: str) -> float:...
+
+    # Returns list of on/off values for stems loaded into channels
+    def get_channel_list_on_off(self) -> List[bool]:...
+
+    # Begins playback
+    def play(self) -> None:...
+
+    # Stops playback
+    def stop(self) -> None:...
